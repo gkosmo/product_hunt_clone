da4040b94af71ffe196d0b7126ebdf4a9c6c920a
g <gkosmo1@hotmail.com>
2019-08-23 10:21:26 +0200

    travis ready

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,15 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+    test "full_name returns the capitalized first name and last name" do
+       user = User.new(first_name: "don", last_name: "trump")
+       assert_equal "Don Trump", user.full_name
+    end
+
+    test "full_name  works with either first name or last name empty " do
+      user1 = User.new(last_name: "obama")
+      assert_equal "Obama", user1.full_name
+      user2 = User.new(first_name: "george")
+      assert_equal "George", user2.full_name
+    end
 end
