3e658de85baf6dda3966a2bfbb67afc58ab16754
george kosmopoulos <gkosmo1@hotmail.com>
2020-08-28 10:24:36 +0200

    added travi

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the #full_name sends back the capitalized full name" do
+    user = User.new(first_name: "john", last_name: "lennon")
+    assert_equal "John Lennon", user.full_name
+  end
 end
